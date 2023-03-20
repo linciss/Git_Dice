0598181494eb1b700ddf46a036cddbead25007af
Sils <linardssils1@gmail.com>
2023-03-20 10:14:02 +0200

    First commit

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Random;
+import java.util.Scanner;
+
+public class Main {
+
+    Random rand = new Random();
+    static Scanner scan = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        System.out.println("Please input the times you want to throw the dice");
+        int times = scan.nextInt();
+        throwDice(times);
+
+    }
+
+    static void throwDice(int times){
+        Random rand = new Random();
+        for(int i =0; i<times;i++)
+            System.out.print(rand.nextInt(6)+1+" ");
+    }
+
+}
